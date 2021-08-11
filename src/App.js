2c35f0ebab4f421d9a51e10485bcf8afbbce8fbc
DianaAnguloM <dianaa985@outlook.com>
2021-08-11 16:52:43 -0600

    conversion de divisas

---
+++ src/App.js
@@ -0,0 +1,60 @@
+import { useState, useEffect } from 'react';
+import './App.css';
+
+
+function App() {
+    const [dolar, setDolar] = useState(0);
+    const [euro, setEuro] = useState(0);
+    const [bitcoin, setBitcoin] = useState(0);
+
+
+    const [tipo, setTipo] = useState(1);
+    const [total, setTotal] = useState(0);
+
+    useEffect(() => {
+        setDolar((total * 1) / tipo);
+        setEuro((total * 1.17) / tipo);
+        setBitcoin((total * 0.000025) / tipo);
+
+    }, [total, tipo]);
+
+
+    const handleTotalChange = e => {
+        if (!isNaN(e.target.value)) {
+            setTotal(e.target.value);
+        }
+    };
+
+
+    return ( <
+        div className = "App" >
+        <
+        h1 > Convertidor de unidades < /h1>
+
+        <
+        p > dolar: { dolar } < /p>  <
+        p > euro: { euro } < /p>  <
+        p > Bitcoin: { bitcoin } < /p>
+
+        <
+        hr / >
+
+        <
+        h3 > { total } < /h3>
+
+        <
+        select onChange = { event => setTipo(event.target.value) }
+        value = { tipo } >
+        <
+        option value = { 1 } > Dolar < /option>  <
+        option value = { 1.17 } > Euro < /option>  <
+        option value = { 0.000025 } > Bitcoin < /option>  <
+        /select>  <
+        input onChange = { handleTotalChange }
+        value = { total }
+        />  <
+        /div>
+    );
+}
+
+export default App;
